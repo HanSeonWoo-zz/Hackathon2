5fa358780c0a6474caf015c2f1b7e6fe567567b8
GranolaReal <skafn25@naver.com>
2020-07-19 18:02:20 +0900

    메인 리사이클러뷰 추가중3

--- app/src/main/java/com/example/hackathon/Main.java
+++ app/src/main/java/com/example/hackathon/Main.java
@@ -19,7 +19,10 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         BottomNavigationInit();
+
     }
 
 
