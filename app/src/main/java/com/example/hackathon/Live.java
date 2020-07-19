f8b700e5061d10a7808ebb466bb0f91b82283ef9
SeonWoo <gkstjsdn10@naver.com>
2020-07-19 16:50:51 +0900

    Live

---
+++ app/src/main/java/com/example/hackathon/Live.java
@@ -0,0 +1,13 @@
+package com.example.hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Live extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
