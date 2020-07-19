f9ed44461e1af80fa29d0aa0f42bf14972739181
JEFFHAN <gkdkxk0943@naver.com>
2020-07-19 18:28:43 +0900

    [추가] 추가,좋아요,프로필 액티비티 / 바텀네비게이션 인텐트 추가

--- app/src/main/java/com/example/hackathon/Main.java
+++ app/src/main/java/com/example/hackathon/Main.java
@@ -19,8 +19,6 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         BottomNavigationInit();
 
     }
