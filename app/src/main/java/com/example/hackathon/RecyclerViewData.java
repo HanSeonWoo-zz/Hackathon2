ff48c509f5203c7eadb575845cbcd6eb296d3662
GranolaReal <skafn25@naver.com>
2020-07-19 17:40:02 +0900

    test파일지움

---
+++ app/src/main/java/com/example/hackathon/RecyclerViewData.java
@@ -0,0 +1,61 @@
+package com.example.hackathon;
+
+import android.graphics.Bitmap;
+
+public class RecyclerViewData {
+
+    private Bitmap userImage;
+    private String userNickName;
+    private Bitmap youTubeThumbnail;
+    private String countLike;
+    private String countView;
+    private String contents;
+
+    public Bitmap getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(Bitmap userImage) {
+        this.userImage = userImage;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public Bitmap getYouTubeThumbnail() {
+        return youTubeThumbnail;
+    }
+
+    public void setYouTubeThumbnail(Bitmap youTubeThumbnail) {
+        this.youTubeThumbnail = youTubeThumbnail;
+    }
+
+    public String getCountLike() {
+        return countLike;
+    }
+
+    public void setCountLike(String countLike) {
+        this.countLike = countLike;
+    }
+
+    public String getCountView() {
+        return countView;
+    }
+
+    public void setCountView(String countView) {
+        this.countView = countView;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
